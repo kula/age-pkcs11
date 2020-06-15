10b95720731dcf9ac9e58cb31f99005ab7bbcad2
Thomas L. Kula <kula@tproa.net>
2020-06-15 08:00:12 -0400

    Split out utility functions

--- age-pkcs11.go
+++ age-pkcs11.go
@@ -15,7 +15,6 @@ import (
     "fmt"
     "io/ioutil"
     "os"
-    "strconv"
     "strings"
     "syscall"
 
@@ -27,49 +26,6 @@ import (
 
     "golang.org/x/crypto/curve25519"
 )
-
-// Given a string like "slot#:token#", return slot and
-// token. Both default to 0. Return error if passed something
-// not a number
-func decode_slot(s string) (int, int, error) {
-    if len(s) == 0 {
-	// Return defaults
-	return 0, 0, nil
-    }
-
-    var sliceString, tokenString string
-    sSlice := strings.Split(s, ":")
-    switch len(sSlice) {
-    case 1:
-	sliceString = sSlice[0]
-	tokenString = "0"
-    case 2:
-	sliceString = sSlice[0]
-	tokenString = sSlice[1]
-    default:
-	return 0,0 , errors.New("bad slot string")
-    }
-
-    slice, err := strconv.Atoi(sliceString)
-    if err != nil {
-	return 0,0 , fmt.Errorf("Bad slot value: %s", slice)
-    }
-
-    token, err := strconv.Atoi(tokenString)
-    if err != nil {
-	return 0, 0, fmt.Errorf("Bad token value: %s", slice)
-    }
-
-    if slice < 0 {
-	return 0, 0, fmt.Errorf("Slice cannot be less than 0")
-    }
-
-    if token < 0 {
-	return 0, 0, fmt.Errorf("Token cannot be less than 0")
-    }
-
-    return slice, token, nil
-}
 
 func main() {
 
